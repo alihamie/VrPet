1d92f2dd0221303db2fec5ec8d18a2135375cf59
Preasul <34010207+Preasul@users.noreply.github.com>
2017-12-30 20:21:25 -0500

    Fixes the 45 degree offset the fox's pawprints had

--- Assets/_VrPetAssets/FoxAssets/Common/Scripts/Utility/StepsManager.cs
+++ Assets/_VrPetAssets/FoxAssets/Common/Scripts/Utility/StepsManager.cs
@@ -11,6 +11,8 @@ namespace MalbersAnimations
         public ParticleSystem Dust;
         public int DustParticles = 30;
 
+        private Quaternion manualOffset = new Quaternion(1f, 3f, 0, 0);
+
         public AudioClip[] clips;
         [Tooltip("Distance to Instantiate the tracks on a terrain")]
         public float trackOffset = 0.0085f;
@@ -37,7 +39,7 @@ namespace MalbersAnimations
                     if (Tracks)
                     {
                         ParticleSystem.EmitParams ptrack = new ParticleSystem.EmitParams();
-                        ptrack.rotation3D = (Quaternion.FromToRotation(-foot.transform.forward, footRay.normal) * foot.transform.rotation).eulerAngles; //Get The Rotation
+                        ptrack.rotation3D = (Quaternion.FromToRotation(-foot.transform.forward, footRay.normal) * foot.transform.rotation * manualOffset).eulerAngles; //Get The Rotation
                         ptrack.position = new Vector3(foot.transform.position.x, footRay.point.y + trackOffset, foot.transform.position.z); //Get The Position
                         Tracks.Emit(ptrack, 1);
                     }
