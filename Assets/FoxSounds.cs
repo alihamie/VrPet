4232e85383604af1e02a257d8020e8db15cef8fe
Preasul <preasul@gmail.com>
2018-06-17 19:04:16 -0400

    This includes the sounds of the fox (on grab and 'Play?'), and the ceiling fan.

---
+++ Assets/FoxSounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxSounds : MonoBehaviour {
+
+    public GameObject foxHead;
+    public float foxVolume = .4f;
+    public AudioClip[] foxClips;
+
+    private AudioSource foxAudio;
+    private string[] sEText;
+
+	// Use this for initialization
+	void Start () {
+        foxAudio = foxHead.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void VoxFox(string startEnd)
+    {
+        sEText = startEnd.Split( );
+        foxAudio.PlayOneShot(foxClips[Random.Range(int.Parse(sEText[0]), int.Parse(sEText[1]))], foxVolume);
+    }
+}
---
+++ Assets/_VrPetAssets/Scripts/AmbientSound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientSound : MonoBehaviour {
+    public AudioClip[] clips;
+    public GameObject startPoint;
+    public GameObject endPoint;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    public int delayTime = 200;
+    public int delayRand = 50;
+    public int delayCount = 200;
+    public float volume = .5f;
+
+    // Use this for initialization
+    void Start () {
+        startPos = startPoint.transform.position;
+        endPos = endPoint.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (delayCount == 0)
+        {
+            AudioSource.PlayClipAtPoint(clips[Random.Range(0, clips.Length)], new Vector3 (Random.Range(startPos.x, endPos.x), Random.Range(startPos.y, endPos.y), Random.Range(startPos.z, endPos.z)), volume);
+            delayCount = delayTime + Random.Range(0, delayRand);
+        }
+        else
+        {
+            delayCount--;
+        }
+	}
+}
