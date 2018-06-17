6b80aee44fa555e386a2ae6ca00ba2b96726239a
Preasul <preasul@gmail.com>
2018-06-16 21:29:35 -0400

    This commit encompasses the bugfixes to the fox's prints, to the car, and to the ceiling. The lightmap was edited to add a single light in the corner, in addition.

---
+++ Assets/Generalized Test Objects/TestAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAudio : MonoBehaviour {
+
+    public AudioClip clipOne, clipTwo;
+    public AudioSource speakerForTheDead;
+    public GameObject iSeeYou;
+    public Vector3 hereIAm;
+    public int theCount;
+    public GameObject foxSayWhat;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hereIAm = new Vector3(iSeeYou.transform.rotation.x, iSeeYou.transform.rotation.y, iSeeYou.transform.rotation.z);
+
+        if (iSeeYou.transform.rotation.x < -.6f && theCount < 1)
+        {
+            AudioSource.PlayClipAtPoint(clipOne, foxSayWhat.transform.position);
+            theCount = 100;
+        }
+        if (theCount > 0)
+        {
+            theCount--;
+        }
+	}
+}
