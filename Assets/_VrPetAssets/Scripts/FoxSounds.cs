4dda7c66c3697a5d2b5fbe6123a5ee01624694fc
Preasul <preasul@gmail.com>
2018-06-18 16:16:33 -0400

    So this is the commit that includes all recommended changes for the pull request. 1#: sEText has been renamed to setText. 2#: Update method has been removed from FoxSounds because it wasn't being used. 3#: The generalized test object and its folder have been removed because of non-use. 4#: AmbientSound.cs now

---
+++ Assets/_VrPetAssets/Scripts/FoxSounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxSounds : MonoBehaviour
+{
+
+    public GameObject foxHead;
+    public float foxVolume = .4f;
+    public AudioClip[] foxClips;
+
+    private AudioSource foxAudio;
+    private string[] setText;
+
+    // Use this for initialization
+    void Start()
+    {
+        foxAudio = foxHead.GetComponent<AudioSource>();
+    }
+
+    public virtual void VoxFox(string startEndClipRange)
+    {
+        setText = startEndClipRange.Split();
+        foxAudio.PlayOneShot(foxClips[Random.Range(int.Parse(setText[0]), int.Parse(setText[1]))], foxVolume);
+    }
+}
