ff01a3c805a6dd6ab838777a3512773a7bde4f25
Preasul <preasul@gmail.com>
2018-06-19 00:53:53 -0400

    This includes changing a few variables to constants in each script, removing superflous comments in the scripts, and re-assigning the proper sounds to the three animations that have them.

--- Assets/_VrPetAssets/Scripts/AmbientSound.cs
+++ Assets/_VrPetAssets/Scripts/AmbientSound.cs
@@ -8,19 +8,17 @@ public class AmbientSound : MonoBehaviour {
     public GameObject endPoint;
     private Vector3 startPos;
     private Vector3 endPos;
-    public float delayTime = 2f;
-    public float delayRand = .5f;
+    public const float delayTime = 2f;
+    public const float delayRand = .5f;
     public float delayCount = 2f;
-    public float volume = .5f;
+    public const float volume = .5f;
 
-    // Use this for initialization
     void Start () {
         delayCount = Time.time + delayCount;
         startPos = startPoint.transform.position;
         endPos = endPoint.transform.position;
     }
 	
-	// Update is called once per frame
 	void Update () {
         if (Time.time > delayCount) {
             AudioSource.PlayClipAtPoint(clips[Random.Range(0, clips.Length)], new Vector3(Random.Range(startPos.x, endPos.x), Random.Range(startPos.y, endPos.y), Random.Range(startPos.z, endPos.z)), volume);
--- Assets/_VrPetAssets/Scripts/FoxSounds.cs
+++ Assets/_VrPetAssets/Scripts/FoxSounds.cs
@@ -6,19 +6,18 @@ public class FoxSounds : MonoBehaviour
 {
 
     public GameObject foxHead;
-    public float foxVolume = .4f;
+    public const float foxVolume = .4f;
     public AudioClip[] foxClips;
 
     private AudioSource foxAudio;
     private string[] setText;
 
-    // Use this for initialization
     void Start()
     {
         foxAudio = foxHead.GetComponent<AudioSource>();
     }
 
-    public virtual void VoxFox(string startEndClipRange)
+    public void VoiceFox(string startEndClipRange)
     {
         setText = startEndClipRange.Split();
         foxAudio.PlayOneShot(foxClips[Random.Range(int.Parse(setText[0]), int.Parse(setText[1]))], foxVolume);
